6e0192d1b73c02bfb509cad774c592991279c9de
Amri Nurfalah <85086059+amrinur@users.noreply.github.com>
2024-06-19 22:10:39 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'thisdarkmode' : '#31363F',
+        'thisdarkmode2' : '#222831',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
